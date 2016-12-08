b2209d3b302fe9c7762ab1f266ebbd1a040bf9cf
EmElw <skallen640@gmail.com>
2016-12-08 15:25:02 +0100

    Changed GameTile into an interface
    Refactored other tiles to implement the new interface, added a BlankTile to preserve old functionality of a base GameTile

---
+++ src/BlankTile.java
@@ -0,0 +1,12 @@
+import java.awt.*;
+
+/**
+ * Created by Magnus on 2016-12-08.
+ */
+class BlankTile implements GameTile {
+    @Override
+    public void draw(Graphics g, int x, int y, Dimension d) {
+//        g.setColor(Color.WHITE);
+//        g.fillRect(x, y, d.width, d.height);
+    }
+}
