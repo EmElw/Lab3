82147e9d32bb3e64ab62ccc401a1629411d8f899
LatteG <gustav.lahti@gmail.com>
2016-12-14 15:46:31 +0100

    Updated GoldModel to work with GameModel and observers. Have a sneaky bug that freezes the program when Gold is launched (no problems launching or playing Reversi), has to be killed with task manager.

--- src/GameController.java
+++ src/GameController.java
@@ -14,38 +14,38 @@ import java.util.List;
 public class GameController implements Runnable, IObservable {
 
 
-  // Helper for handling property changes
-  private PropertyChangeSupport observers;
+    // Helper for handling property changes
+    private PropertyChangeSupport observers;
 
-  /**
-   * The view this controller is connected to.
-   */
-  private final GameView view;
+    /**
+     * The view this controller is connected to.
+     */
+    private final GameView view;
 
-  /**
-   * The game model describes the running game.
-   */
-  private GameModel gameModel;
+    /**
+     * The game model describes the running game.
+     */
+    private GameModel gameModel;
 
-  /**
-   * The timeout interval between each update. (millis)
-   */
-  private final int updateInterval;
+    /**
+     * The timeout interval between each update. (millis)
+     */
+    private final int updateInterval;
 
-  /**
-   * True when game is running.
-   */
-  private boolean isRunning;
+    /**
+     * True when game is running.
+     */
+    private boolean isRunning;
 
-  /**
-   * Listener for key events to the game.
-   */
-  private final KeyListener keyListener;
+    /**
+     * Listener for key events to the game.
+     */
+    private final KeyListener keyListener;
 
-  /**
-   * A queue for all keypresses which so far haven't been processed
-   */
-  private final List<Integer> keypresses;
+    /**
+     * A queue for all keypresses which so far haven't been processed
+     */
+    private final List<Integer> keypresses;
 
 	/*
    * The declaration of keypresses above uses the language feature 'generic
@@ -55,142 +55,142 @@ public class GameController implements Runnable, IObservable {
 	 * going to be used in the List
 	 */
 
-  /**
-   * The thread which the game runs in.
-   */
-  private Thread gameThread;
+    /**
+     * The thread which the game runs in.
+     */
+    private Thread gameThread;
 
-  /**
-   * Creats a new GameContoller associated with supplied view.
-   */
-  public GameController(final GameView view) {
-    this.view = view;
-    this.gameModel = null;
-    this.isRunning = false;
-    this.updateInterval = 150;
+    /**
+     * Creats a new GameContoller associated with supplied view.
+     */
+    public GameController(final GameView view) {
+        this.view = view;
+        this.gameModel = null;
+        this.isRunning = false;
+        this.updateInterval = 150;
 
-    this.keypresses = new LinkedList<Integer>();
+        this.keypresses = new LinkedList<>();
 
-    this.gameThread = null;
+        this.gameThread = null;
 
-    // Create the key listener which will listen for gamekeys
-    this.keyListener = new KeyAdapter() {
-      @SuppressWarnings("synthetic-access")
-      @Override
-      public void keyPressed(final KeyEvent event) {
-        enqueueKeyPress(event.getKeyCode());
-      }
-    };
+        // Create the key listener which will listen for gamekeys
+        this.keyListener = new KeyAdapter() {
+            @SuppressWarnings("synthetic-access")
+            @Override
+            public void keyPressed(final KeyEvent event) {
+                enqueueKeyPress(event.getKeyCode());
+            }
+        };
 
-  }
-
-  /**
-   * Add a key press to the end of the queue
-   */
-  private synchronized void enqueueKeyPress(final int key) {
-    this.keypresses.add(Integer.valueOf(key));
-  }
-
-  /**
-   * Get a key press, and remove it from the queue. Returns 0 if no key press
-   * is available.
-   *
-   * @return 0 or next unprocessed key press.
-   */
-  private synchronized int nextKeyPress() {
-    if (this.keypresses.isEmpty()) {
-      return 0;
-    }
-    return this.keypresses.remove(0).intValue();
-  }
-
-  /**
-   * Starts a new game.
-   *
-   * @param gameModel Game to start
-   */
-  public void startGame(final GameModel gameModel) {
-    if (this.isRunning) {
-      throw new IllegalStateException("Game is already running");
     }
 
-    // Start listening for key events
-    this.view.addKeyListener(this.keyListener);
+    /**
+     * Add a key press to the end of the queue
+     */
+    private synchronized void enqueueKeyPress(final int key) {
+        this.keypresses.add(Integer.valueOf(key));
+    }
 
-    // Tell the view what to paint...
-    this.view.setModel(gameModel);
-
-    // Actually start the game
-    this.gameModel = gameModel;
-    this.isRunning = true;
-    this.gameModel.addObserver(view);
-
-
-    // Create the new thread and start it...
-    this.gameThread = new Thread(this);
-    this.gameThread.start();
-  }
-
-  /**
-   * Stops the currently running game, if any.
-   */
-  public void stopGame() {
-    // Setting isRunning to false will
-    // make the thread stop (see run())
-    this.isRunning = false;
-
-    // Unset the game model...
-    this.view.setModel(null);
-
-    // Stop listening for events
-    this.view.removeKeyListener(this.keyListener);
-
-    // Make sure we wait until the thread has stopped...
-    if (this.gameThread != null) {
-      while (this.gameThread.isAlive()) {
-        try {
-          Thread.sleep(100);
-        } catch (InterruptedException ie) {
-          // Pass the call on.
-          Thread.currentThread().interrupt();
+    /**
+     * Get a key press, and remove it from the queue. Returns 0 if no key press
+     * is available.
+     *
+     * @return 0 or next unprocessed key press.
+     */
+    private synchronized int nextKeyPress() {
+        if (this.keypresses.isEmpty()) {
+            return 0;
         }
-      }
+        return this.keypresses.remove(0).intValue();
     }
-  }
 
-  /**
-   * This code runs the game in a different thread.
-   */
-  @Override
-  public void run() {
-    while (this.isRunning) {
-      try {
-        // Tell model to update, send next key press.
-        // or 0 if no new keypress since last update.
-        this.gameModel.gameUpdate(nextKeyPress());
+    /**
+     * Starts a new game.
+     *
+     * @param gameModel Game to start
+     */
+    public void startGame(final GameModel gameModel) {
+        if (this.isRunning) {
+            throw new IllegalStateException("Game is already running");
+        }
+
+        // Start listening for key events
+        this.view.addKeyListener(this.keyListener);
+
+        // Tell the view what to paint...
+        this.view.setModel(gameModel);
+
+        // Actually start the game
+        this.gameModel = gameModel;
+        this.isRunning = true;
+        this.gameModel.addObserver(view);
+
+
+        // Create the new thread and start it...
+        this.gameThread = new Thread(this);
+        this.gameThread.start();
+    }
+
+    /**
+     * Stops the currently running game, if any.
+     */
+    public void stopGame() {
+        // Setting isRunning to false will
+        // make the thread stop (see run())
+        this.isRunning = false;
+
+        // Unset the game model...
+        this.view.setModel(null);
+
+        // Stop listening for events
+        this.view.removeKeyListener(this.keyListener);
+
+        // Make sure we wait until the thread has stopped...
+        if (this.gameThread != null) {
+            while (this.gameThread.isAlive()) {
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException ie) {
+                    // Pass the call on.
+                    Thread.currentThread().interrupt();
+                }
+            }
+        }
+    }
+
+    /**
+     * This code runs the game in a different thread.
+     */
+    @Override
+    public void run() {
+        while (this.isRunning) {
+            try {
+                // Tell model to update, send next key press.
+                // or 0 if no new keypress since last update.
+                this.gameModel.gameUpdate(nextKeyPress());
 
 //        observers.firePropertyChange("KeyPress",0,nextKeyPress());
 
-        Thread.sleep(this.updateInterval);
-      } catch (GameOverException e) {
-        // we got a game over signal, time to exit...
-        // The current implementation ignores the game score
-        this.isRunning = false;
-        System.out.println("Game over: " + e.getScore());
-      } catch (InterruptedException e) {
-        // if we get this exception, we're asked to terminate ourselves
-        this.isRunning = false;
-      }
+                Thread.sleep(this.updateInterval);
+            } catch (GameOverException e) {
+                // we got a game over signal, time to exit...
+                // The current implementation ignores the game score
+                this.isRunning = false;
+                System.out.println("Game over: " + e.getScore());
+            } catch (InterruptedException e) {
+                // if we get this exception, we're asked to terminate ourselves
+                this.isRunning = false;
+            }
+        }
     }
-  }
 
-  @Override
-  public void addObserver(PropertyChangeListener observer) {
-    observers.addPropertyChangeListener(observer);
-  }
+    @Override
+    public void addObserver(PropertyChangeListener observer) {
+        observers.addPropertyChangeListener(observer);
+    }
 
-  @Override
-  public void removeObserver(PropertyChangeListener observer) {
-    observers.removePropertyChangeListener(observer);
-  }
+    @Override
+    public void removeObserver(PropertyChangeListener observer) {
+        observers.removePropertyChangeListener(observer);
+    }
 }
--- src/GameView.java
+++ src/GameView.java
@@ -1,12 +1,7 @@
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.Font;
-import java.awt.Graphics;
-import java.awt.Image;
+import javax.swing.*;
+import java.awt.*;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
-
-import javax.swing.JComponent;
 
 /**
  * A view Component suitable for inclusion in an AWT Frame. Paints itself by
@@ -14,117 +9,117 @@ import javax.swing.JComponent;
  */
 public class GameView extends JComponent implements PropertyChangeListener {
 
-  /**
-   * Size of game model
-   */
-  private final Dimension modelSize;
+    /**
+     * Size of game model
+     */
+    private final Dimension modelSize;
 
-  /**
-   * Size of every tile in the model
-   */
-  private final Dimension tileSize;
+    /**
+     * Size of every tile in the model
+     */
+    private final Dimension tileSize;
 
-  /**
-   * The game model which is drawn
-   */
-  private GameModel model;
+    /**
+     * The game model which is drawn
+     */
+    private GameModel model;
 
-  /**
-   * The offscreen buffer
-   */
-  private Graphics offscreenGraphics;
+    /**
+     * The offscreen buffer
+     */
+    private Graphics offscreenGraphics;
 
-  /**
-   * Image representing the offscreen graphics
-   */
-  private Image offscreenImage;
+    /**
+     * Image representing the offscreen graphics
+     */
+    private Image offscreenImage;
 
-  /**
-   * Creates a view where each GameObject has side length 40 pixels..
-   */
-  public GameView() {
-    this(40);
-  }
-
-  /**
-   * Creates a view where each GameObject has a given size.
-   *
-   * @param tileSide side length in pixels of each GameObject.
-   */
-  public GameView(final int tileSide) {
-    this.tileSize = new Dimension(tileSide, tileSide);
-    this.modelSize = Constants.getGameSize();
-    Dimension preferredSize =
-            new Dimension(this.modelSize.width * tileSide,
-                    this.modelSize.height * tileSide);
-    setPreferredSize(preferredSize);
-  }
-
-  /**
-   * Updates the view with a new model.
-   */
-  public void setModel(final GameModel model) {
-    this.model = model;
-    repaint();
-  }
-
-  /**
-   * This method ensures that the painting is performed double-buffered. This
-   * means there won't be any flicker when repainting all the time.
-   */
-  @Override
-  public void update(final Graphics g) {
-    // Create an offscreen buffer (if we don't have one)
-    if (this.offscreenImage == null) {
-      Dimension size = getSize();
-
-      this.offscreenImage = createImage(size.width, size.height);
-      this.offscreenGraphics = this.offscreenImage.getGraphics();
+    /**
+     * Creates a view where each GameObject has side length 40 pixels..
+     */
+    public GameView() {
+        this(40);
     }
 
-    // This will invoke painting correctly on the offscreen buffer
-    super.update(this.offscreenGraphics);
+    /**
+     * Creates a view where each GameObject has a given size.
+     *
+     * @param tileSide side length in pixels of each GameObject.
+     */
+    public GameView(final int tileSide) {
+        this.tileSize = new Dimension(tileSide, tileSide);
+        this.modelSize = Constants.getGameSize();
+        Dimension preferredSize =
+                new Dimension(this.modelSize.width * tileSide,
+                        this.modelSize.height * tileSide);
+        setPreferredSize(preferredSize);
+    }
 
-    // Draw the contents of the offscreen buffer to screen.
-    g.drawImage(this.offscreenImage, 0, 0, this);
-  }
+    /**
+     * Updates the view with a new model.
+     */
+    public void setModel(final GameModel model) {
+        this.model = model;
+        repaint();
+    }
 
-  /**
-   * Consults the model to paint the game matrix. If model is null, draws a
-   * default text.
-   */
+    /**
+     * This method ensures that the painting is performed double-buffered. This
+     * means there won't be any flicker when repainting all the time.
+     */
+    @Override
+    public void update(final Graphics g) {
+        // Create an offscreen buffer (if we don't have one)
+        if (this.offscreenImage == null) {
+            Dimension size = getSize();
 
-  @Override
-  public void paintComponent(final Graphics g) {
-    // Check if we have a running game
-    super.paintComponent(g);
-    g.setColor(this.getBackground());
-    g.fillRect(0, 0, getWidth(), getHeight());
-
-    if (this.model != null) {
-
-      // Draw all tiles by going over them x-wise and y-wise.
-      for (int i = 0; i < this.modelSize.width; i++) {
-        for (int j = 0; j < this.modelSize.height; j++) {
-          // Draw each layer of the tile
-          for (GameTile tile : this.model.getGameboardState(i, j)) {
-            if (tile != null)
-              tile.draw(g, i * this.tileSize.width, j
-                              * this.tileSize.height,
-                      this.tileSize);
-          }
+            this.offscreenImage = createImage(size.width, size.height);
+            this.offscreenGraphics = this.offscreenImage.getGraphics();
         }
-      }
-    } else {
-      g.setFont(new Font("Sans", Font.BOLD, 24));
-      g.setColor(Color.BLACK);
-      final char[] message = "No model chosen.".toCharArray();
-      g.drawChars(message, 0, message.length, 50, 50);
-    }
-  }
 
-  @Override
-  public void propertyChange(PropertyChangeEvent evt) {
-    repaint();
-  }
+        // This will invoke painting correctly on the offscreen buffer
+        super.update(this.offscreenGraphics);
+
+        // Draw the contents of the offscreen buffer to screen.
+        g.drawImage(this.offscreenImage, 0, 0, this);
+    }
+
+    /**
+     * Consults the model to paint the game matrix. If model is null, draws a
+     * default text.
+     */
+
+    @Override
+    public void paintComponent(final Graphics g) {
+        // Check if we have a running game
+        super.paintComponent(g);
+        g.setColor(this.getBackground());
+        g.fillRect(0, 0, getWidth(), getHeight());
+
+        if (this.model != null) {
+
+            // Draw all tiles by going over them x-wise and y-wise.
+            for (int i = 0; i < this.modelSize.width; i++) {
+                for (int j = 0; j < this.modelSize.height; j++) {
+                    // Draw each layer of the tile
+                    for (GameTile tile : this.model.getGameboardState(i, j)) {
+                        if (tile != null)
+                            tile.draw(g, i * this.tileSize.width,
+                                    j * this.tileSize.height,
+                                    this.tileSize);
+                    }
+                }
+            }
+        } else {
+            g.setFont(new Font("Sans", Font.BOLD, 24));
+            g.setColor(Color.BLACK);
+            final char[] message = "No model chosen.".toCharArray();
+            g.drawChars(message, 0, message.length, 50, 50);
+        }
+    }
+
+    @Override
+    public void propertyChange(PropertyChangeEvent evt) {
+        repaint();
+    }
 }
